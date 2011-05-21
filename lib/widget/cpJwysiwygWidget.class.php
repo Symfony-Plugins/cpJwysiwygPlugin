927d967265b02927802cde2fdc07efc8c914086c
coopers <coopers@ee427ae8-e902-0410-961c-c3ed070cd9f9>
2011-05-21 09:09:24 -0500

    Merge branch 'master' into svn
    git-svn-id: http://svn.symfony-project.com/plugins/cpJwysiwygPlugin/trunk@32573 ee427ae8-e902-0410-961c-c3ed070cd9f9

---
+++ lib/widget/cpJwysiwygWidget.class.php
@@ -0,0 +1,42 @@
+<?php
+class cpJwysiwygWidget extends sfWidgetFormTextArea {
+
+  protected function configure($options = array(), $attributes = array()) {
+    $this->addOption('template', <<<EOF
+%widget%
+<script type="text/javascript">
+  jQuery(document).ready(function() {
+    jQuery('#%id%').wysiwyg();
+  });
+</script>    
+EOF
+);
+  }
+
+  /**
+   * @param  string $name        The element name
+   * @param  string $value       The value displayed in this widget
+   * @param  array  $attributes  An array of HTML attributes to be merged with the default HTML attributes
+   * @param  array  $errors      An array of errors for the field
+   *
+   * @return string An HTML tag string
+   *
+   * @see sfWidgetForm
+   */
+  public function render($name, $value = null, $attributes = array(), $errors = array()) {
+    $id = $this->generateId($name);
+
+    return strtr($this->getOption('template'), array(
+             '%widget%' => parent::render($name, $value, $attributes, $errors),
+             '%id%' => $id
+            ));
+  }
+  
+  public function getJavascripts() {
+    return array('/dzJwysiwygPlugin/jwysiwyg/jquery.wysiwyg.js');
+  }
+  
+  public function getStylesheets() {
+    return array('/dzJwysiwygPlugin/jwysiwyg/jquery.wysiwyg.css' => 'screen');
+  }
+}
